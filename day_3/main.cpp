6862b7547207083c63437a9924f34f7aa03c8e5e
sulicat <suli.barakat@gmail.com>
2021-11-14 12:41:47 -0500

    Test

--- day_3/main.cpp
+++ day_3/main.cpp
@@ -1,32 +1,59 @@
 #include "graphics.h"
-
 #include <iostream>
 
 
+class Particle{
+public:
+    Particle()
+    {
+        size = 5;
+        r = 255;
+        g = 0;
+        b = 255;
+        a = 255;
+        x = 200;
+        y = 200;
+    }
 
-int main( int argc, char** argv )
+    ~Particle(){}
+
+    float size;
+    int r;
+    int g;
+    int b;
+    int a;
+    float x;
+    float y;
+};
+
+
+int main( )
 {
     Graphics graphics(800, 800);
-    static float angle = 0;
+    float angle = 0;
+
+
+    std::vector<Particle> list_of_parts;
+
+    for(int i = 0; i < 100; i++ )
+    {
+        Particle my_particle = Particle();
+        list_of_parts.push_back( my_particle );
+    }
+
+    std::cout << list_of_parts.size() << "\n";
 
     while( graphics.is_running() )
     {
+        graphics.clear();
 
-	graphics.clear();
+        for(int i = 0; i < list_of_parts.size(); i++)
+        {
+            graphics.circle( list_of_parts[i].x, list_of_parts[i].y, list_of_parts[i].size,
+                             list_of_parts[i].r, list_of_parts[i].g, list_of_parts[i].b, list_of_parts[i].a,
+                             false );
+        }
 
-	if( graphics.is_L_mouse_down() )
-	{
-
-	    graphics.rect( graphics.get_mouse_x(),
-			   graphics.get_mouse_y(),
-			   20, 20,
-			   angle,
-			   255, 0, 255, 255,
-			   true );
-
-	    angle += 1;
-	}
-
-	graphics.draw();
+        graphics.draw();
     }
 }
